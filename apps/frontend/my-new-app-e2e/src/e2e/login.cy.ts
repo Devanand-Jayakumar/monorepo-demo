47ac6cc50b0ee38bfbd6ff5ed53862cd92ca94e6
Kesavarajan Rajapandian <kesavarajan.rajapandian@informa.com>
2025-08-13 16:48:34 +0530

    Update login.cy.ts

--- apps/frontend/my-new-app-e2e/src/e2e/login.cy.ts
+++ apps/frontend/my-new-app-e2e/src/e2e/login.cy.ts
@@ -4,7 +4,7 @@ describe('@org-informa/my-new-app-e2e', () => {
   beforeEach(() => cy.visit('https://visitcloud.com/contact-visit/'));
 
   it('should display welcome message', () => {
-    // Custom command example, see `../support/commands.ts`
+    // Custom command example, see `../support/commands.ts` file
     cy.log('my-email@something.com', 'myPassword');
 
     // Function helper example, see `../support/app.po.ts` file
